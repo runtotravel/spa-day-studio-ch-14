946fb5e48feb1f041aba1f49290afb93bcfb8abb
Trisha <trishaposey@gmail.com>
2023-05-08 19:51:15 -0500

    finished studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,24 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @NotNull
+    @Size(min=5, max=15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank
+    @NotNull
+    @Size(min=6)
     private String password;
+
 
     public User() {
 
